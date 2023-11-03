262b6c5e6b4aeb65b93c20b181650f454cb62671
biswa7064 <biswaranjan16856@gmail.com>
2023-11-04 00:05:19 +0530

    test: successfully run the test case for App.ts

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,9 +1,27 @@
 import App from "../src/App"
 import { render } from "@testing-library/react"
+import useHotels from "../src/hooks/useHotels"
 
-jest.mock("../src/hooks/useHotels")
+jest.mock("../src/hooks/useHotels.ts")
+const mockUseHotels = useHotels as jest.MockedFunction<typeof useHotels>
+
+const mockHotelsData = {
+	hotels: [
+		{ hotelID: "1", address: "Hotel 1", ratings: 4, image: "" },
+		{ hotelID: "2", address: "Hotel 2", ratings: 5, image: "" },
+	],
+}
 
 describe(App, () => {
+	beforeEach(() => {
+		jest.clearAllMocks()
+		mockUseHotels.mockReturnValue({
+			hotelsData: mockHotelsData,
+		} as any)
+	})
+	afterAll(() => {
+		jest.resetAllMocks()
+	})
 	it("should correctly render App", () => {
 		const { container } = render(<App />)
 		const AppElement = container.getElementsByClassName("App")
