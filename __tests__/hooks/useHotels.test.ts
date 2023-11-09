b039cf39fa5d253a6cc78b3bee1fd1268b5f74d5
biswa7064 <biswaranjan16856@gmail.com>
2023-11-10 01:13:44 +0530

    test: Success scenario for useHotels hook

--- __tests__/hooks/useHotels.test.ts
+++ __tests__/hooks/useHotels.test.ts
@@ -1,14 +1,49 @@
+import * as ApolloClient from "@apollo/client"
 import useHotels from "../../src/hooks/useHotels"
+import { ADD_HOTEL, GET_HOTELS } from "src/graphql/queries/hotelQueries"
+
+jest.mock("@apollo/client")
+let spyMutation = jest.spyOn(ApolloClient, "useMutation")
+let spyQuery = jest.spyOn(ApolloClient, "useQuery")
+const mockAddHotel = jest.fn()
+
+const mockReturnedValue: ReturnType<typeof useHotels> = {
+	getHotelsLoading: false,
+	getHotelsError: undefined,
+	hotelsData: [] as any,
+	addedHotel: {},
+	addHotel: jest.fn(),
+	addHotelError: undefined,
+	addHotelLoading: false,
+}
 
 describe(useHotels, () => {
 	beforeEach(() => {
 		jest.clearAllMocks()
+		spyMutation.mockImplementation(
+			jest
+				.fn()
+				.mockReturnValue([
+					mockAddHotel,
+					{ loading: false, data: {}, error: undefined },
+				])
+		)
+		spyQuery.mockImplementation(
+			jest.fn().mockReturnValue({ loading: false, data: [], error: undefined })
+		)
 	})
 	afterEach(() => {})
 	afterAll(() => {
-		jest.restoreAllMocks()
+		jest.resetAllMocks()
 	})
-	it("should give truthy value", () => {
-		expect(true).toBeTruthy()
+	it("should successfully return from hook", () => {
+		const data = useHotels()
+		expect(spyMutation).toHaveBeenCalled()
+		expect(spyMutation).toHaveBeenCalledWith(ADD_HOTEL)
+		expect(spyQuery).toHaveBeenCalled()
+		expect(spyQuery).toHaveBeenCalledWith(GET_HOTELS)
+		expect(JSON.stringify(data)).toStrictEqual(
+			JSON.stringify(mockReturnedValue)
+		)
 	})
 })
