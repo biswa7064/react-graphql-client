403c15039557245fa7fff9ee8d1389f6ad3c5279
biswa7064 <biswaranjan16856@gmail.com>
2023-11-14 12:24:19 +0530

    test: Complete test cases for useHotels hook

--- __tests__/hooks/useHotels.test.ts
+++ __tests__/hooks/useHotels.test.ts
@@ -1,6 +1,7 @@
 import * as ApolloClient from "@apollo/client"
 import useHotels from "../../src/hooks/useHotels"
 import { ADD_HOTEL, GET_HOTELS } from "src/graphql/queries/hotelQueries"
+import { renderHook } from "@testing-library/react"
 
 jest.mock("@apollo/client")
 let spyMutation = jest.spyOn(ApolloClient, "useMutation")
@@ -12,7 +13,7 @@ const mockReturnedValue: ReturnType<typeof useHotels> = {
 	getHotelsError: undefined,
 	hotelsData: [] as any,
 	addedHotel: {},
-	addHotel: jest.fn(),
+	addHotel: mockAddHotel,
 	addHotelError: undefined,
 	addHotelLoading: false,
 }
@@ -37,13 +38,39 @@ describe(useHotels, () => {
 		jest.resetAllMocks()
 	})
 	it("should successfully return from hook", () => {
-		const data = useHotels()
+		const { result } = renderHook(useHotels)
 		expect(spyMutation).toHaveBeenCalled()
 		expect(spyMutation).toHaveBeenCalledWith(ADD_HOTEL)
 		expect(spyQuery).toHaveBeenCalled()
 		expect(spyQuery).toHaveBeenCalledWith(GET_HOTELS)
-		expect(JSON.stringify(data)).toStrictEqual(
+		expect(JSON.stringify(result.current)).toStrictEqual(
 			JSON.stringify(mockReturnedValue)
 		)
+	})
+
+	it("should return correct response based on query call", () => {
+		const expectedHookResponse = {
+			...mockReturnedValue,
+			getHotelsLoading: true,
+			addHotelLoading: true,
+		}
+		spyMutation.mockImplementation(
+			() => [mockAddHotel, { loading: true, data: {}, error: undefined }] as any
+		)
+		spyQuery.mockImplementation(
+			jest.fn().mockReturnValue({
+				loading: true,
+				data: [],
+				error: undefined,
+			})
+		)
+		const { result } = renderHook(useHotels)
+		expect(JSON.stringify(result.current)).toStrictEqual(
+			JSON.stringify(expectedHookResponse)
+		)
+		expect(result.current.addHotelLoading).toEqual(true)
+		expect(result.current.getHotelsLoading).toEqual(true)
+		expect(spyMutation).toHaveBeenCalled()
+		expect(spyQuery).toHaveBeenCalled()
 	})
 })
